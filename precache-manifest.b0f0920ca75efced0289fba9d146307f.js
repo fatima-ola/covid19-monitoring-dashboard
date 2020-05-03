fc067a1e0311ac77c046660584f22bdf8d0227e3
fatima-ola <fatimaolasunkanmi01@gmail.com>
2020-05-03 22:25:10 +0100

    Updates

---
+++ precache-manifest.b0f0920ca75efced0289fba9d146307f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f198db15ed30dfea1b7d98c4e015eb",
+    "url": "/covid19-monitoring-dashboard/index.html"
+  },
+  {
+    "revision": "514295cee3292a755616",
+    "url": "/covid19-monitoring-dashboard/static/css/main.8dea26a6.chunk.css"
+  },
+  {
+    "revision": "6b863a9017cacb6296ed",
+    "url": "/covid19-monitoring-dashboard/static/js/2.d0137cb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-monitoring-dashboard/static/js/2.d0137cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514295cee3292a755616",
+    "url": "/covid19-monitoring-dashboard/static/js/main.c5533ec3.chunk.js"
+  },
+  {
+    "revision": "85efcb7b16f71eb31be3",
+    "url": "/covid19-monitoring-dashboard/static/js/runtime-main.1d4895ee.js"
+  }
+]);
